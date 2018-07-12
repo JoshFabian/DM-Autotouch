cd13b5a8d358f46cad3f7fa3d0633b1e879f3873
Josh <contact@joshfabian.com>
2018-07-12 12:05:57 -0400

    Update win/loss counter

--- DM Arena.lua
+++ DM Arena.lua
@@ -1,130 +1,178 @@
 require "DM.dmconfig"
 require "DM.dmutils"
 
+local mmr = 0;
+
 while (1==1) do
  -- Begin Arena Farm
  -- -- -- --
 
   -- Enter Arena
-  if getColor(76,1904) == 14393094 then
-     log("Entered arena.");
+  if hasPixel(menu.enterArena) then
      tap(101,1922);
      usleep(500000);
   end
 
-  -- Begin Arena Fight
-  if getColor(68,2235) == 14656006 then
-     log("Arena fight initiated.");
-     tap(102,2160);
-     usleep(500000);
+  -- Check Enemy Blacklist
+  if hasPixels(blackList.dassvakten)
+  or hasPixels(blackList.fartville)
+  or hasPixels(blackList.piupap)
+  or hasPixels(blackList.sharqu)
+  or hasPixels(blackList.luckyAsian)
+  or hasPixels(blackList.asgard)
+  or hasPixels(blackList.duchp86)
+  or hasPixels(blackList.ukAsian)
+  or hasPixels(blackList.willpla4food)
+  or hasPixels(blackList.cityTheCats)
+  or hasPixels(blackList.admiral)
+  or hasPixels(blackList.ribsy)
+  or hasPixels(blackList.botBlock)
+  or hasPixels(blackList.wilmington)
+  or hasPixels(blackList.sony) then
+    if hasPixel(menu.skipFight) then
+      tap(233,2156);
+      usleep(1000000);
+      tap(1061,162); -- Necessary until avatar bug is fixed
+      usleep(1000000);
+    end
+  else
+    -- Start Fight
+    if hasPixel(menu.startFight) then
+       tap(102,2160);
+       usleep(500000);
+    end
   end
 
-  -- Golden Tiamat
-  if hasPixel(Heroes.godArrow) then
-    log("Used Golden Tiamat's God Arrow.");
-    thirdAttack();
-  else
-    if hasPixel(Heroes.holyStorm) then
-      log("Used Golden Tiamat's Holy Storm.");
+
+  while hasPixel(screen.arena) do
+
+    -- Black Mask
+    if hasPixel(blackMask.bombTossActive) then
+      thirdAttack();
+    else
+      if hasPixel(blackMask.bombTossInactive) then
+        thirdAttack();
+        firstAttack();
+      end
+    end
+
+    -- Umbra
+    if hasPixel(ursusUmbra.overdriveActive) then
       firstAttack();
     end
-  end
 
-  -- Umbra
-  if hasPixel(Heroes.overdrive) then
-    log("Used Umbra's Overdrive.");
-    secondAttack();
-  else
-    if hasPixel(Heroes.rainOfBlades) then
-      log("Used Umbra's Rain of Blades.");
+    -- Ivy Elf Warden
+    -- if hasPixel(ivyElfWarden.seedBarrageActive) then
+    --   secondAttack();
+    -- else
+    --   if hasPixel(ivyElfWarden.seedBarrageInactive) then
+    --     firstAttack();
+    --   end
+    -- end
+
+    -- Rolanda
+    if hasPixel(rolanda.rallyCallActive) then
+      secondAttack();
+    else
+      if hasPixel(rolanda.lionsRoarActive) then
+        secondAttack();
+        thirdAttack();
+      else
+        if hasPixel(rolanda.lionsRoarInactive) then
+          secondAttack();
+          firstAttack();
+        end
+      end
+    end
+
+    -- Legend Tim
+    if hasPixel(vaultMasterTimothy.infernoBlitzActive) then
       fourthAttack();
     else
-      if hasPixel(Heroes.electroShock) then
-        log("Used Umbra's Electro Shock.");
+      if hasPixel(vaultMasterTimothy.infernoBlitzInactive) then
+        fourthAttack();
         firstAttack();
       end
     end
-  end
 
-  -- Vault Keeper
-  if hasPixel(Heroes.renewal) then
-    log("Used Vault Keeper's Renewal.");
-    fourthAttack();
-  else
-    if hasPixel(Heroes.vaultBlast) then
-      log("Used Vault Keeper's Vault Blast.");
-      thirdAttack();
-    else
-      if hasPixel(Heroes.vaultSlash) then
-        log("Used Vault Keeper's Vault Slash.");
-        firstAttack();
-      end
-    end
-  end
+    -- Vault Keeper
+    -- if hasPixel(vaultKeeper.renewalActive) then
+    --   fourthAttack();
+    -- else
+    --   if hasPixel(vaultKeeper.vaultBlastActive) then
+    --     thirdAttack();
+    --   else
+    --     if hasPixel(vaultKeeper.renewalInactive)  then
+    --       firstAttack();
+    --     end
+    --   end
+    -- end
 
-  -- Wyld Troll
-  if hasPixel(Heroes.barkBlowout) then
-    log("Used Wyld Troll's Bark Blowout.");
-    secondAttack();
-  else
-    if hasPixel(Heroes.entangle) then
-      log("Used Wyld Troll's Entangle");
-      thirdAttack();
-    else
-      if hasPixel(Heroes.knockOut) then
-        log("Used Wyld Troll's Knockout");
-        firstAttack();
-      end
-    end
-  end
+    -- Wyld Troll
+    -- if hasPixel(wyldTroll.barkBlowoutActive) then
+    --   secondAttack();
+    -- else
+    --   if hasPixel(wyldTroll.entangleActive) then
+    --     thirdAttack();
+    --   else
+    --     if hasPixel(wyldTroll.entangleInactive) then
+    --       firstAttack();
+    --     end
+    --   end
+    -- end
 
-  -- Results Screen
-  if getColor(124,1229) == 4030374 then
-     -- Close Victory Screen
-     log("We won! Closing results screen.");
-     tap(96,572);
-     usleep(500000);
-  else
-     if getColor(124,1229) == 5602451 then
-       -- Close Defeat Screen
-       log("We Lost... Closing results screen.");
-       tap(100,192);
+    -- Herbalist
+    -- if hasPixel(ivyElfHerbalist.overgrowthActive) then
+    --   thirdAttack();
+    -- else
+    --   if hasPixel(ivyElfHerbalist.herbalismActive) then
+    --     secondAttack();
+    --   else
+    --     if hasPixel(ivyElfHerbalist.herbalismInactive) then
+    --       firstAttack();
+    --     end
+    --   end
+    -- end
+
+    -- Results Screen
+    if hasPixel(menu.victoryResult) then
+       -- Close Victory Screen
+       mmr=mmr+5
+       log(string.format("We won : %d", mmr));
+       tap(96,572);
        usleep(500000);
-     end
+    else
+       if hasPixel(menu.defeatResult) then
+         -- Close Defeat Screen
+         mmr=mmr-40
+         log(string.format("We Lost : %d", mmr));
+         tap(100,192);
+         usleep(500000);
+       end
+    end
   end
 
---   -- Open Reward Chests
---   local results = findColors({{8069152,0,0}, {1973018,-12,16}, {13603340,-12,27}}, 0, nil);
---   if results ~= nil then
---     log("Opened reward chest.");
---     for i, a in pairs(results) do
---       tap(a[1], a[2])
---       usleep(5000)
---       tap(a[1], a[2])
---       usleep(5000)
---     end
---   end
---
---   -- Close Reward Screen
---   local results = findColors({{2630974,0,0}, {16040806,-9,-4}, {14524968,-20,-5}}, 0, nil);
---   if results ~= nil then
---     log("Collected chest rewards, back to arena.");
---     for i, a in pairs(results) do
---       tap(a[1], a[2])
---       usleep(30000)
---       tap(a[1], a[2])
---       usleep(30000)
---     end
---   end
---
---   -- Daily Login
---   if getColor(955, 2097) == 7799812 then
---      -- Close Daily Screen
---      log("Collected daily login rewards, back to arena.");
---      tap(948, 2112);
---      usleep(30000);
---   end
---
---   -- End Arena Farm
---   -- -- -- --
+  -- Open Reward Chest
+  if hasPixel(menu.openRewardChest) then
+    log("Opened reward chest.");
+    tap(140,939);
+    usleep(500000);
+  end
+
+  -- Close Reward Chest
+  if hasPixel(menu.closeRewardChest) or hasPixel(menu.closeRewardChestAlt)  then
+    log("Closed reward chest.");
+    tap(112,2284);
+    usleep(500000);
+  end
+
+  -- Close Daily Popup
+  if hasPixel(menu.closeDaily) then
+    log("Closed reward chest.");
+    tap(172, 1271);
+    usleep(500000);
+  end
+
+-- End Arena Farm
+-- -- -- --
 end
